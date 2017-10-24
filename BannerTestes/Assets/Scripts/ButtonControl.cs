6e368db1ff54bc1ca6493dfbfe84d4a0557c5aed
Rafael Trindade <rst2trindade@gmail.com>
2017-10-24 18:50:47 -0400

    Criado os butões no imagem target

---
+++ BannerTestes/Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonControl : MonoBehaviour
+{
+    public GameObject animChamander;
+
+    public void clickStart()
+    {
+        animChamander.SetActive(true);        
+    }
+
+    public void clickStop()
+    {
+        animChamander.SetActive(false);
+    }
+
+
+}
