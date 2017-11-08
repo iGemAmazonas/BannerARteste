d0d66ff6809073ebe0b88c1941179503a3212110
rafael trindade <rst2trindade@gmail.com>
2017-11-08 16:06:17 -0400

    Criado AR Software

---
+++ BannerTestes/Assets/Scripts/ScriptSoftware/GifNext.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GifNext : MonoBehaviour {
+    public Animator gifAnimator;
+
+    public void gifUp()
+    {
+        gifAnimator.SetTrigger("ClicUP");
+    }
+    public void gifDown()
+    {
+        gifAnimator.SetTrigger("ClicDown");
+    }
+    public void reloding()
+    {
+  
+    }
+
+}
---
+++ BannerTestes/Assets/Scripts/ScriptSoftware/Teste.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teste : MonoBehaviour
+{
+    public void teste ()
+    {
+        Debug.Log("testeBotao");
+    }
+
+}
